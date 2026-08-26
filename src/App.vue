aa46f222e04f80bf4f021ac09eefa62f5bb0acb5
Nico <nicolas.venegas@gmail.com>
2026-08-26 12:39:22 -0400

    create repo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup> 
+const name = "anarres";
+const styleColor = "color: blue";
+const arrayColores = ["red", "blue", "green"];
+const activo = false;
 </script>
 
 <template>
-  <HelloWorld />
+<h1>Welcome to {{ name.toUpperCase() }}</h1>
+<h2 :style="`color: ${arrayColores[1]}`">hombre azul</h2>
+<h2>
+  {{ activo ? "activo" : "inactivo" }}
+</h2>
 </template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
